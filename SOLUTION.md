b12efda395de03c9a9467ca82824e35d67b9f9ca
Ankur <ydv.ankur1992@gmail.com>
2019-04-16 00:31:57 +0200

    solution

--- SOLUTION.md
+++ SOLUTION.md
@@ -1,3 +1,6 @@
 # Solution Docs
 
-<!-- You can include documentation, additional setup instructions, notes etc. here -->
+This is my solution and i have taken a assumptions like : 
+
+1) you are always gonna use the api url in format this 'https://api.github.com/search/users?q=${query}' where ${query} will be replaced by the input.
+2) If you press up arrow key when you are in dropdown the selection will start from last element in dropdown
--- index.js
+++ index.js
@@ -15,10 +15,10 @@ new Autocomplete(document.getElementById('state'), {
   },
 });
 
-
 // Github Users
-// new Autocomplete(document.getElementById('gh-user'), {
-//   onSelect: (ghUserId) => {
-//     console.log('selected github user id:', ghUserId);
-//   },
-// });
+new Autocomplete(document.getElementById('gh-user'), {
+  url: 'https://api.github.com/search/users?q=${query}&per_page=${numOfResults}',
+  onSelect: (ghUserId) => {
+    console.log('selected github user id:', ghUserId);
+  },
+});
